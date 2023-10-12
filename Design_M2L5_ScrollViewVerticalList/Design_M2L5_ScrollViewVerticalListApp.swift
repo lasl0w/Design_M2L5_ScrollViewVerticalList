cd6333a21d50a11a6760ef78b133d5b99aa487f8
Tom Montgomery <montgomery3681@gmail.com>
2023-10-11 22:12:39 -0600

    Initial Commit

---
+++ Design_M2L5_ScrollViewVerticalList/Design_M2L5_ScrollViewVerticalListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_M2L5_ScrollViewVerticalListApp.swift
+//  Design_M2L5_ScrollViewVerticalList
+//
+//  Created by tom montgomery on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Design_M2L5_ScrollViewVerticalListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
