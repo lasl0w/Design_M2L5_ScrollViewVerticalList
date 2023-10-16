3b6ecae468870802f81367af688f698824f4abcb
Tom Montgomery <montgomery3681@gmail.com>
2023-10-15 22:22:35 -0600

    - scrollview & lists

--- Design_M2L5_ScrollViewVerticalList/ContentView.swift
+++ Design_M2L5_ScrollViewVerticalList/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var items = ["cat","dog","mouse","panther"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        // Scrollviews automatically handle shifting elements when showing/hiding the onscreen keyboard
+        
+        //            VStack {
+        //                Image(systemName: "globe")
+        //                    .imageScale(.large)
+        //                    .foregroundStyle(.tint)
+        //                Text("Hello, world!")
+        //            }
+        //            .padding()
+        // List is automatically a scrollview
+        // BENEFITS - row seperators AND Swipe actions
+        // TODO: How have lists changed in XCode 15
+        
+        // METHOD 1 - trailing closure
+        List(items, id: \.self) { item in
+            Text(item)
         }
-        .padding()
+        
+        
+        // METHOD 2 - FOREACH
+        List {
+            ForEach(items, id: \.self) { item in
+                Text(item)
+            }
+        }
+        
+        // METHOD 3 - Scrollview + Custom VStack
+        ScrollView {
+            VStack {
+                // starts with less, but has more custom control
+                ForEach(items, id: \.self) { item in
+                    Text(item)
+                    
+                }
+            }
+        }
+        
+        
+        
     }
 }
 
